9252bae166e29dd02e663597f409b171cf8fbf21
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2015-08-19 18:58:36 -0400

    Added PushWoosh plugins -updated js/html

--- cordova/platforms/android/assets/www/cordova_plugins.js
+++ cordova/platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/com.pushwoosh.plugins.pushwoosh/www/PushNotification.js",
+        "id": "com.pushwoosh.plugins.pushwoosh.PushNotification",
+        "clobbers": [
+            "plugins.pushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-device": "1.0.2-dev",
-    "org.apache.cordova.console": "0.2.13"
+    "org.apache.cordova.console": "0.2.13",
+    "com.pushwoosh.plugins.pushwoosh": "3.6.2"
 }
 // BOTTOM OF METADATA
 });
